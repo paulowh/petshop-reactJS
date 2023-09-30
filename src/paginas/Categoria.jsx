01dc7c0a34eebd2041c8f293ffa64e7b1b0bc88c
Paulo Henrique <ph744915@gmail.com>
2023-09-30 13:48:47 -0300

    listando as subcategorias

--- src/paginas/Categoria.jsx
+++ src/paginas/Categoria.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
-import { Route, Routes, useParams } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, Route, Routes, useParams } from 'react-router-dom';
 import '../assets/css/blog.css'
 import ListaCategorias from '../components/ListaCategorias';
 import ListPost from '../components/ListPost';
+import { busca } from '../api/api';
 
 
 const Categoria = () => {
     const { id } = useParams()
+
+    const [subcategorias, setSubCategorias] = useState([])
+
+    useEffect(() => {
+        busca(`/categorias/${id}`, (categoria) => {
+            setSubCategorias(categoria.subcategorias)
+        })
+    }, [id])
 
     return (
         <main>
@@ -14,6 +23,19 @@ const Categoria = () => {
                 <h2 className='titulo-pagina'>Pet Notícias</h2>
             </div>
             <ListaCategorias />
+
+            <ul className="lista-categorias container flex">
+                {
+                    subcategorias.map((subcategoria) => (
+                        <Link>
+                            <li className={`lista-categorias__categoria lista-categorias__categoria--${id}`}>
+                                {subcategoria}
+                            </li>
+                        </Link>
+                    ))
+                } 
+            </ul>
+
 
             <Routes>
                 <Route path={`/`} element={<ListPost url={`/posts?categoria=${id}`} />} />
